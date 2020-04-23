730a0abca92b2068c67d1ab5cd1f4c25e7f146ad
Mahmood Hoseini <mah.hoseini2005@gmail.com>
2020-04-22 17:55:46 -0700

    Add files via upload

---
+++ w3_assignment_inv.R
@@ -0,0 +1,24 @@
+makeCacheMatrix <- function(x=matrix()) {
+    iv <- NULL
+    set <- function(y) {
+        x <- y
+        iv <- NULL
+    }
+    get <- function() x
+    setinv <- function(new_inv) iv <- new_inv
+    getinv <- function() iv
+    list(set=set, get=get, getinv=getinv, setinv=setinv)
+}
+
+
+cacheSolve <- function(x, ...) {
+    iv <- x$getinv()
+    if (!is.null(iv)) {
+        message('Cache inverse is used!')
+        return(iv)
+    }
+    data <- x$get()
+    iv <- solve(data) %*% data
+    x$setinv(iv)
+    iv
+}
